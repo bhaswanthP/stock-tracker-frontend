20e139965602189a127ab5bd24902d0f4bdad872
Bhaswanth Polamarasetti <144129081+bhaswanthP@users.noreply.github.com>
2025-07-27 15:20:40 +0530

    Changed back to vercel url for fetch

--- src/pages/AddAlert.jsx
+++ src/pages/AddAlert.jsx
@@ -13,7 +13,7 @@ const AddAlert = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://34.235.157.206:3010/api/alerts", {
+      const res = await fetch("https://stock-tracker-ruby.vercel.app/api/alerts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, symbol, targetPrice }),
